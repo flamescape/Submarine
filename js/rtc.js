4242e05aa14e5dde1664c163e4a5e74b510baed3
John Ballantyne <johnballantyne@gmail.com>
2014-02-08 09:24:14 -0500

    Bit of refactoring

--- js/rtc.js
+++ js/rtc.js
@@ -1,40 +1,52 @@
 var conn;
 var peer = new Peer({key: "t1oqjc5cdoenrk9"});
+var txt = document.getElementById("txt");
+var url = document.getElementById("url");
 
+var recieveData = function(data) {
+    txt.innerHTML = data;
+};
 
-peer.on("open", function(id) {
-    document.getElementById("url").setAttribute("value", window.location.origin + "/#" + id);
-});
+var connected = function() {
+    url.setAttribute("value", "Connected!");
+};
 
-peer.on('connection', function(c) {
-    conn = c;
-    conn.on('data', function(data) {
-        var txt = document.getElementById("txt");
-        txt.innerHTML = data;
-    });
-});
-
+//Initialize the connection
 if (window.location.hash !== "") {
-    console.log("We have hash");
+    //Player 2
+    url.setAttribute("value", "Conntecting...");
     conn = peer.connect(window.location.hash.substr(1));
+
     conn.on('open', function() {
-        conn.on('data', function(data) {
-            var txt = document.getElementById("txt");
-            txt.innerHTML = data;
-        });
+        connected();
+        conn.on('data', recieveData);
+    });
+} else {
+    //Player 1
+    peer.on("open", function(id) {
+        url.setAttribute("value", window.location.origin + "/#" + id);
+    });
+
+    peer.on('connection', function(c) {
+        connected();
+        conn = c;
+        conn.on('data', recieveData);
     });
 }
 
-var callback = function() {
+//Send message and clear text box
+var sendMessage = function() {
     var msg = document.getElementById("msg");
     conn.send(msg.value);
     msg.value = "";
 };
 
+//Bind button to send message
 var form = document.getElementById("button");
 
 if (form.addEventListener) {
-    form.addEventListener("click", callback, false);
+    form.addEventListener("click", sendMessage, false);
 } else if (form.attachEvent) {
-    form.attachEvent("onclick", callback);
+    form.attachEvent("onclick", sendMessage);
 }
+
