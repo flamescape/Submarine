5597c1468eed406be9373dcd6340f3488dc97c86
John Ballantyne <johnballantyne@gmail.com>
2014-02-07 22:36:07 -0500

    Amazing chat feature

--- js/rtc.js
+++ js/rtc.js
@@ -1,14 +1,40 @@
+var conn;
 var peer = new Peer({key: "t1oqjc5cdoenrk9"});
 
 
 peer.on("open", function(id) {
-    console.log("Hey your partner should go to " + window.location.origin + "/#" + id);
+    document.getElementById("url").setAttribute("value", window.location.origin + "/#" + id);
 });
 
-if (window.location.hash !== undefined) {
-    var conn = peer.connect(window.location.hash.substr(1));
+peer.on('connection', function(c) {
+    conn = c;
+    conn.on('data', function(data) {
+        var txt = document.getElementById("txt");
+        txt.innerHTML = data;
+    });
+});
+
+if (window.location.hash !== "") {
+    console.log("We have hash");
+    conn = peer.connect(window.location.hash.substr(1));
+    conn.on('open', function() {
+        conn.on('data', function(data) {
+            var txt = document.getElementById("txt");
+            txt.innerHTML = data;
+        });
+    });
 }
 
-peer.on('connection', function(conn) {
-    console.log("HELLO THERE!!!!");
-});
+var callback = function() {
+    var msg = document.getElementById("msg");
+    conn.send(msg.value);
+    msg.value = "";
+};
+
+var form = document.getElementById("button");
+
+if (form.addEventListener) {
+    form.addEventListener("click", callback, false);
+} else if (form.attachEvent) {
+    form.attachEvent("onclick", callback);
+}
