ca757b2114596a802911e7c2e7f0201459f6d736
John Ballantyne <johnballantyne@gmail.com>
2014-02-07 20:55:42 -0500

    Let's interact

--- js/rtc.js
+++ js/rtc.js
@@ -2,6 +2,13 @@ var peer = new Peer({key: "t1oqjc5cdoenrk9"});
 
 
 peer.on("open", function(id) {
-    console.log("My peer ID is.... " + id);
+    console.log("Hey your partner should go to " + window.location.origin + "/#" + id);
 });
 
+if (window.location.hash !== undefined) {
+    var conn = peer.connect(window.location.hash.substr(1));
+}
+
+peer.on('connection', function(conn) {
+    console.log("HELLO THERE!!!!");
+});
