1e9e87acf5702db42d79c19998249a3c4a5711ab
Mikuso <g_github@flamescape.com>
2014-02-08 23:02:42 +0000

    x

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,37 +1,21 @@
 
 var Gameboard = function(x,y,z){
-    //create grid
+    var board = this;
     
+    // create grid
+    var group = BABYLON.Mesh.CreatePlane("", 0, Game.scene);
+    group.position = new BABYLON.Vector3(0,0,0);
     
-    var boxCorner = BABYLON.Mesh.CreateBox("Box", 0.2, Game.scene);
-    boxCorner.position = new BABYLON.Vector3(0,0,0);
-    
-    this.grid = this.createGrid(x,y,z, Game.SPACE_EMPTY);
-    this.gridGraphics = this.createGrid(x,y,z, function(x,y,z){
-        var materialEmptyCube = new BABYLON.StandardMaterial("texture1", Game.scene);
-        materialEmptyCube.alpha = 1 - (0.2 * y);
-        materialEmptyCube.diffuseColor = new BABYLON.Color3(1, 0, 1);
-        var box = BABYLON.Mesh.CreateBox("Box", 0.9, Game.scene);
-        box.position = new BABYLON.Vector3(x,y,z);
-        box.material = materialEmptyCube;
-        box.parent = boxCorner;
-        return box;
+    this.grid = this.createGrid(5,5,5,function(x,y,z){
+        return board.createEmptyCube(x,y,z,group);
     });
     
-    var boxCorner = BABYLON.Mesh.CreateBox("Box", 0.2, Game.scene);
-    boxCorner.position = new BABYLON.Vector3(-10,0,0);
+    var group = BABYLON.Mesh.CreatePlane("", 0, Game.scene);
+    group.position = new BABYLON.Vector3(-10,0,0);
     
-    this.grid = this.createGrid(x,y,z, Game.SPACE_EMPTY);
-    this.gridGraphics = this.createGrid(x,y,z, function(x,y,z){
-        var materialEmptyCube = new BABYLON.StandardMaterial("texture1", Game.scene);
-        materialEmptyCube.alpha = 1 - (0.2 * y);
-        var box = BABYLON.Mesh.CreateBox("Box", 0.9, Game.scene);
-        box.position = new BABYLON.Vector3(x,y,z);
-        box.material = materialEmptyCube;
-        box.parent = boxCorner;
-        return box;
+    this.grid = this.createGrid(5,5,5,function(x,y,z){
+        return board.createEmptyCube(x,y,z,group);
     });
-    
     
 };
 
@@ -42,11 +26,23 @@ Gameboard.prototype.createGrid = function(x,y,z,what){
         for (var yy = 0; yy < y; yy++) {
             var c = [];
             for (var zz = 0; zz < z; zz++) {
-                c[zz] = _.isFunction(what) ? what(xx,yy,zz) : what;
+                c[zz] = what(xx,yy,zz);
             }
             b[yy] = c;
         }
         a[xx] = b;
     }
     return a;
+};
+
+Gameboard.prototype.createEmptyCube = function(x,y,z,parent){
+    var materialEmptyCube = new BABYLON.StandardMaterial("texture1", Game.scene);
+    materialEmptyCube.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    
+    var box = BABYLON.Mesh.CreateBox("Box", 0.9, Game.scene);
+    box.position = new BABYLON.Vector3(x,y,z);
+    box.material = materialEmptyCube;
+    box.parent = parent;
+    
+    return box;
 };
