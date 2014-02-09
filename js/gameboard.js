1101b151793179c13ecf0ef18e4e2b1abcbafe1f
Mikuso <g_github@flamescape.com>
2014-02-09 00:49:27 +0000

    Boxes

--- js/gameboard.js
+++ js/gameboard.js
@@ -3,18 +3,11 @@ var Gameboard = function(x,y,z){
     var board = this;
     
     // create grid
-    var group = BABYLON.Mesh.CreatePlane("", 0, Game.scene);
-    group.position = new BABYLON.Vector3(0,0,0);
+    this.group = BABYLON.Mesh.CreatePlane("", 0, Game.scene);
+    this.group.position = new BABYLON.Vector3(0,0,0);
     
     this.grid = this.createGrid(5,5,5,function(x,y,z){
-        return board.createEmptyCube(x,y,z,group);
-    });
-    
-    var group = BABYLON.Mesh.CreatePlane("", 0, Game.scene);
-    group.position = new BABYLON.Vector3(-10,0,0);
-    
-    this.grid = this.createGrid(5,5,5,function(x,y,z){
-        return board.createEmptyCube(x,y,z,group);
+        return board.createEmptyCube(x,y,z,board.group);
     });
     
 };
@@ -39,7 +32,7 @@ Gameboard.prototype.createEmptyCube = function(x,y,z,parent){
     var materialEmptyCube = new BABYLON.StandardMaterial("texture1", Game.scene);
     materialEmptyCube.diffuseColor = new BABYLON.Color3(1, 0, 0);
     
-    var box = BABYLON.Mesh.CreateSphere("Box", 16, 0.9, Game.scene);
+    var box = BABYLON.Mesh.CreateBox("Box", 0.9, Game.scene);
     box.position = new BABYLON.Vector3(x,y,z);
     box.material = materialEmptyCube;
     box.parent = parent;
